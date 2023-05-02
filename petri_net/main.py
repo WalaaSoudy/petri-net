bf7434bffaf3d0b2f9a79f664cc4b0f115ecdb57
Walaa soudy <11410120201218@stud.cu.edu.eg>
2023-05-02 13:09:17 +0300

    edit

--- petri_net/main.py
+++ petri_net/main.py
@@ -1,106 +1,120 @@
-from collections import deque
+'''
+This code defines a class called PetriNet that represents a Petri Net,
+which is a type of directed graph used for modeling concurrent systems.
+The class has the following attributes: places (a list of strings representing the places in the Petri Net), transitions
+(a list of strings representing the transitions in the Petri Net), flows (a list of tuples representing the arcs in the Petri Net)
+,and initial_marking (a dictionary representing the initial marking of the Petri Net).
 
-class Place:
-    def __init__(self, id, tokens):
-        self.id = id
-        self.tokens = tokens
+The PetriNet class also has two methods: is_sound and generate_reachability_graph.
+The is_sound method checks if the Petri Net is sound by verifying that all transitions have at least one input place.
+The generate_reachability_graph method generates the reachability graph of the Petri Net,
+which is a directed graph representing all possible states of the Petri Net.
 
-class Transition:
-    def __init__(self, id, input_places, output_places):
-        self.id = id
-        self.input_places = input_places
-        self.output_places = output_places
+The code prompts the user to input the Petri Net details such as the places,
+transitions, flows, and initial marking, and creates a PetriNet object. It then checks if the Petri Net is sound
+and generates the reachability graph. If the user chooses to draw the reachability graph,
+it uses the NetworkX library and Matplotlib to visualize the graph. If the user chooses not to draw the graph,
+it prints the reachability set for each node.
 
-class Flow:
-    def __init__(self, input_place_id, output_place_id, weight):
-        self.input_place_id = input_place_id
-        self.output_place_id = output_place_id
-        self.weight = weight
-def initialize_petri_net(places, transitions, flows, initial_marks):
-    # Initialize the places with the given initial marks
-    for i in range(len(places)):
-        places[i].tokens = initial_marks[i]
+To run the code,
+save it as a Python file (e.g., petri_net.py) and run it using a Python interpreter.
+The interpreter will prompt the user to input the Petri Net details,
+and the code will generate and display the reachability graph if the user chooses to do so.
+'''
 
-def can_fire_transition(transition, places):
-    # Check if the input places have enough tokens and the output places have room for more tokens
-    for input_place_id in transition.input_places:
-        if places[input_place_id].tokens < 1:
-            return False
-    for output_place_id in transition.output_places:
-        if places[output_place_id].tokens >= 1:
-            return False
-    return True
+import networkx as nx
+import matplotlib.pyplot as plt
 
-def fire_transition(transition, places):
-    # Remove tokens from input places and add tokens to output places
-    for input_place_id in transition.input_places:
-        places[input_place_id].tokens -= 1
-    for output_place_id in transition.output_places:
-        places[output_place_id].tokens += 1
+class PetriNet:
+    def __init__(self, places, transitions, flows, initial_marking):
+        self.places = places
+        self.transitions = transitions
+        self.flows = flows
+        self.initial_marking = initial_marking
 
-def generate_reachability_graph(places, transitions, flows, initial_marks):
-    state_queue = deque()
-    visited_states = set()
+    def is_sound(self):
+        inputs = {}
+        outputs = {}
 
-    # Initialize the queue with the initial state
-    initial_state = [place.tokens for place in places]
-    state_queue.append(initial_state)
-    visited_states.add(tuple(initial_state))
+        # Iterate over all flows and populate inputs and outputs
+        for flow in self.flows:
+            if flow[1] not in inputs:
+                inputs[flow[1]] = set()
+            inputs[flow[1]].add(flow[0])
 
-    while len(state_queue) > 0:
-        current_state = state_queue.popleft()
+            if flow[0] not in outputs:
+                outputs[flow[0]] = set()
+            outputs[flow[0]].add(flow[1])
 
-        # Generate all possible successor states by firing enabled transitions
-        for transition in transitions:
-            if can_fire_transition(transition, [Place(place.id, place.tokens) for place in places]):
-                successor_state = current_state.copy()
-                fire_transition(transition, [Place(place.id, place.tokens) for place in places])
-                successor_state = [place.tokens for place in places]
+        # Check if there are any transitions that have no inputs
+        for t in self.transitions:
+            if t not in inputs:
+                return False
 
-                # Enqueue the successor state if it has not been visited before
-                if tuple(successor_state) not in visited_states:
-                    state_queue.append(successor_state)
-                    visited_states.add(tuple(successor_state))
+        return True
 
-    return visited_states
+    def generate_reachability_graph(self):
+        # We will use NetworkX library to generate the reachability graph.
+        G = nx.DiGraph()
+        for place in self.places:
+            G.add_node(place, label=place)
+        for transition in self.transitions:
+            G.add_node(transition, label=transition)
+        for flow in self.flows:
+            G.add_edge(flow[0], flow[1], weight=1)
+        # Compute the reachability set for each node using depth-first search.
+        for node in G.nodes:
+            reachability_set = set()
+            self.dfs(node, reachability_set, G)
+            G.nodes[node]['reachability_set'] = reachability_set
+        return G
 
-def is_sound(places, transitions, flows, initial_marks):
-    reachability_graph = generate_reachability_graph(places, transitions, flows, initial_marks)
-    return len(reachability_graph) < float('inf')
+    def dfs(self, node, reachability_set, G):
+        reachability_set.add(node)
+        for neighbor in G.neighbors(node):
+            if neighbor not in reachability_set:
+                self.dfs(neighbor, reachability_set, G)
 
-# Take user input for Petri net configuration
-n_places = int(input("Enter the number of places: "))
-places = []
-for i in range(n_places):
-    tokens = int(input(f"Enter the number of tokens in place {i}: "))
-    places.append(Place(i, tokens))
-
-n_transitions = int(input("Enter the number of transitions: "))
-transitions = []
-for i in range(n_transitions):
-    input_places = list(map(int, input(f"Enter the input places for transition {i} (comma separated): ").split(',')))
-    output_places = list(map(int, input(f"Enter the output places for transition {i} (comma separated): ").split(',')))
-    transitions.append(Transition(i, input_places, output_places))
-
-n_flows = int(input("Enter the number of flows: "))
+# Take input for Petri Net
+print("Please input the Petri Net:")
+places = input("Places: ").split(",")
+transitions = input("Transitions: ").split(",")
+flows_input = input("Flows (format: place1->transition1,transition1->place2,...): ")
 flows = []
-for i in range(n_flows):
-    input_place_id, output_place_id, weight = map(int, input(f"Enter the input place, output place and weight for flow {i} (comma separated): ").split(','))
-    flows.append(Flow(input_place_id, output_place_id, weight))
+for flow in flows_input.split(","):
+    flow_parts = flow.split("->")
+    flows.append((flow_parts[0], flow_parts[1]))
+print(flows)  # Debugging line
+initial_marking_input = input("Initial marking (format: place1=marking1,place2=marking2,...): ")
+initial_marking = {}
+for marking in initial_marking_input.split(","):
+    marking_parts = marking.split("=")
+    initial_marking[marking_parts[0]] = int(marking_parts[1])
 
-initial_marks = list(map(int, input("Enter the initial marks for each place (comma separated): ").split(',')))
+# Create Petri net object
+pn = PetriNet(places, transitions, flows, initial_marking)
 
-# Initialize the Petri net with the given configuration
-initialize_petri_net(places, transitions, flows, initial_marks)
-
-# Check if the Petri net is sound
-if is_sound(places, transitions, flows, initial_marks):
-    print("Petri net is sound")
+# Check soundness
+if pn.is_sound():
+    print("Petri is sound")
 else:
-    print("Petri net is not sound")
+    print("Petri Net is not sound.")
 
-# Generate the reachability graph for the Petri net
-reachability_graph = generate_reachability_graph(places, transitions, flows, initial_marks)
-print("Reachability graph:")
-for state in reachability_graph:
-    print(state)
+
+G = pn.generate_reachability_graph()
+
+# Draw or print reachability graph
+if input("Do you want to draw the reachability graph? (y/n) ") == "y":
+    pos = nx.spring_layout(G)
+    node_labels = nx.get_node_attributes(G, 'label')
+    edge_labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_nodes(G, pos, nodelist=G.nodes(), node_color='r')
+    nx.draw_networkx_labels(G, pos, labels=node_labels)
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color='b')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    plt.show()
+else:
+    for node in G.nodes:
+        print(node, G.nodes[node]['reachability_set'])
+
+
